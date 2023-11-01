0a113f0cc4727aaa0dfb199ff2814c81efb87c8c
JNocetti <juan.anocetti@gmail.com>
2023-11-01 17:10:00 -0300

    update

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ filas = int(input("Ingrese el número de filas: "))
 columnas = int(input("Ingrese el número de columnas: "))
 matriz = utils.crearMatriz(filas, columnas)
 
+print("Matriz inicial:")
 print(matriz)
+
+solucion = utils.resolver_lights_out(matriz)
+print("Solución:")
+print(solucion)
+
+
--- utils.py
+++ utils.py
@@ -3,4 +3,19 @@ import numpy as np
 def crearMatriz(filas, columnas):
     return np.random.randint(2, size=(filas, columnas))
 
+def resolver_lights_out(matriz_inicial):
+    n = len(matriz_inicial)
+    solucion = np.zeros((n, n), dtype=int)
+
+    for fila in range(n):
+        for columna in range(n):
+            suma_vecinos = matriz_inicial[fila, columna]
+            suma_vecinos += fila > 0 and matriz_inicial[fila - 1, columna]
+            suma_vecinos += fila < n - 1 and matriz_inicial[fila + 1, columna]
+            suma_vecinos += columna > 0 and matriz_inicial[fila, columna - 1]
+            suma_vecinos += columna < n - 1 and matriz_inicial[fila, columna + 1]
+
+            solucion[fila, columna] = suma_vecinos % 2
+
+    return solucion.flatten()
     
